768fcd1bb067a9c58651a28e979e1a80eb791fc5
recebimentof31-spec <recebimentof31@gmail.com>
2026-08-05 21:46:59 +0000

    Primeira versão do AutoLog ERP

---
+++ js/app.js
@@ -0,0 +1,21 @@
+function login(){
+
+    let usuario = document.getElementById("usuario").value;
+
+    let senha = document.getElementById("senha").value;
+
+    if(usuario=="admin" && senha=="1234"){
+
+        window.location.href = "dashboard.html";
+
+    
+
+    }
+
+    else{
+
+        document.getElementById("mensagem").innerHTML="Usuário ou senha incorretos.";
+
+    }
+
+}
